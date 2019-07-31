4a0696bcc9291956a21c38e66ebe576df7f45769
Travis Gee <geet084@gmail.com>
2019-07-31 15:21:58 -0600

    Sets up environments for knex

---
+++ knexfile.js
@@ -0,0 +1,39 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/travelr-be',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/development'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/db_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true,
+  },
+  
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/development'
+    }
+  },
+
+};
