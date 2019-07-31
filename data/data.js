68393fa6e69d42b5f615058ac42fdc791b5f505c
Travis Gee <geet084@gmail.com>
2019-07-31 17:23:38 -0600

    Updates data file to remove nested specs

--- data/data.js
+++ data/data.js
@@ -1,30 +1,26 @@
 module.exports = [
   {
     "object_name": "Sun",
-    "specs": {
-      "average_temp": "5778 K",
-      "distance_from_sun": "0 km",
-      "diameter": "1392000 km",
-      "images": [],
-      "length_of_day": "587 hours",
-      "number_of_moons": 0,
-      "moon": false,
-      "orbital_period": "225000000 years",
-      "planet": false,
-    }
+    "average_temp": "5778 K",
+    "distance_from_sun": "0 km",
+    "diameter": "1392000 km",
+    "images": ['img1', 'img2'],
+    "length_of_day": "587 hours",
+    "number_of_moons": 0,
+    "moon": false,
+    "orbital_period": "225000000 years",
+    "planet": false,
   },
   {
     "object_name": "Moon",
-    "specs": {
-      "average_temp": "235 K",
-      "distance_from_sun": "147,000,000 / 152,000,000 km",
-      "diameter": "3475 km",
-      "images": [],
-      "length_of_day": "708 hours",
-      "number_of_moons": 0,
-      "moon": true,
-      "orbital_period": "27 days",
-      "planet": false,
-    }
+    "average_temp": "235 K",
+    "distance_from_sun": "147,000,000 / 152,000,000 km",
+    "diameter": "3475 km",
+    "images": ['img1', 'img2', 'img3', 'img4'],
+    "length_of_day": "708 hours",
+    "number_of_moons": 0,
+    "moon": true,
+    "orbital_period": "27 days",
+    "planet": false,
   }
 ];
