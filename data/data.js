518ad64faa398e82ed072079568e39e781e39d95
Travis Gee <geet084@gmail.com>
2019-07-31 14:08:45 -0600

    Adds initial data file

---
+++ data/data.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    "object_name": "Sun",
+    "specs": {
+      "average_temp": "5778 K",
+      "distance_from_sun": "0 km",
+      "diameter": "1392000 km",
+      "images": [],
+      "length_of_day": "587 hours",
+      "number_of_moons": 0,
+      "moon": false,
+      "orbital_period": "225000000 years",
+      "planet": false,
+    }
+  },
+  {
+    "object_name": "Moon",
+    "specs": {
+      "average_temp": "235 K",
+      "distance_from_sun": "147,000,000 / 152,000,000 km",
+      "diameter": "3475 km",
+      "images": [],
+      "length_of_day": "708 hours",
+      "number_of_moons": 0,
+      "moon": true,
+      "orbital_period": "27 days",
+      "planet": false,
+    }
+  }
+];
