6cd178f1338f1a067485e70dd910203dc037775e
Travis Gee <geet084@gmail.com>
2019-07-31 15:41:36 -0600

    Adds tables for objects and images

--- db/migrations/20190731151955_initial.js
+++ db/migrations/20190731151955_initial.js
@@ -1,8 +1,33 @@
+exports.up = (knex, Promise) => {
+  return Promise.all([
+    knex.schema.createTable('objects', table => {
+      table.increments('id').primary();
+      table.string('object_name');
+      table.string('average_temp');
+      table.string('distance_from_sun');
+      table.string('diameter');
+      table.string('length_of_day');
+      table.integer('number_of_moons');
+      table.boolean('moon');
+      table.string('orbital_period');
+      table.boolean('planet');
+      table.timestamps(true, true);
+    }),
 
-exports.up = function(knex) {
-  
+    knex.schema.createTable('images', table => {
+      table.increments('id').primary();
+      table.string('image_id');
+      table.integer('object_id').unsigned()
+      table.foreign('object_id')
+        .references('objects.id');
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
-exports.down = function(knex) {
-  
+exports.down = (knex, Promise) => {
+  return Promise.all([
+    knex.schema.dropTable('images'),
+    knex.schema.dropTable('objects')
+  ]);
 };
