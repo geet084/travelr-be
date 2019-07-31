c11b8179125de9472bc00c562fcff09de4b3a3d6
Travis Gee <geet084@gmail.com>
2019-07-31 15:25:23 -0600

    Connects server environment to knex

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(cors());
 app.use(express.json());
