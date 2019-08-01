94bcfdd099926924d6f4f1b2afc95608f4419b33
Travis Gee <geet084@gmail.com>
2019-08-01 15:37:43 -0600

    Adds test env seed file

---
+++ db/seeds/test/objects.js
@@ -0,0 +1,45 @@
+const objectsData = require('../../../data/data');
+
+const createImage = (knex, image) => {
+  return knex('images').insert(image)
+};
+
+const createObject = (knex, object) => {
+  return knex('objects').insert({
+    object_name: object.object_name,
+
+    average_temp: object.average_temp,
+    distance_from_sun: object.distance_from_sun,
+    diameter: object.diameter,
+    length_of_day: object.length_of_day,
+    number_of_moons: object.number_of_moons,
+    moon: object.moon,
+    orbital_period: object.orbital_period,
+    planet: object.planet,
+  }, 'id')
+    .then(objectId => {
+      let imagesPromises = [];
+      object.images.forEach(image => {
+        imagesPromises.push(
+          createImage(knex, {
+            image_id: image,
+            object_id: objectId[0]
+          })
+        )
+      })
+      return Promise.all(imagesPromises)
+    })
+}
+
+exports.seed = (knex) => {
+  return knex('images').del()
+    .then(() => knex('objects').del())
+    .then(() => {
+      let objectPromises = [];
+      objectsData.forEach(object => {
+        objectPromises.push(createObject(knex, object))
+      })
+      return Promise.all(objectPromises);
+    })
+    .catch(error => console.log(`Error seeding data ${error}`));
+};
