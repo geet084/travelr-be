fa1f629d7578be7470ab0cf2199307776c46dab9
Travis Gee <geet084@gmail.com>
2019-08-01 12:56:24 -0600

    Adds initial endpoints to verify data set

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const database = require('knex')(configuration);
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/v1/test', (req, res) => {
-  res.status(200).json('initial hookup successful')
+app.get('/api/v1/objects', (req, res) => {
+  database('objects').select()
+    .then(objects => res.status(200).json(objects))
+    .catch(error => res.status(500).json({ error }))
+})
+
+app.get('/api/v1/images', (req, res) => {
+  database('images').select()
+    .then(images => res.status(200).json(images))
+    .catch(error => res.status(500).json({ error }))
 })
 
 module.exports = app;
