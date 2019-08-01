6a5db806d1d7e2ad43144e0da4d3538a6141e06b
Travis Gee <geet084@gmail.com>
2019-08-01 14:33:38 -0600

    Sets up test file

---
+++ app.spec.js
@@ -0,0 +1,6 @@
+const request = require('supertest');
+const app = require('./app');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const objects = require('./data/data');
