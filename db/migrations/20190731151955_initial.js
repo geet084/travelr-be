8d678821d73540fc1f95f1ddad954a24a401bc49
Travis Gee <geet084@gmail.com>
2019-08-05 17:10:03 -0600

    Updates migration table naming and properties

--- db/migrations/20190731151955_initial.js
+++ db/migrations/20190731151955_initial.js
@@ -2,21 +2,25 @@ exports.up = (knex) => {
   return Promise.all([
     knex.schema.createTable('objects', table => {
       table.increments('id').primary();
-      table.string('object_name');
+      table.string('name');
       table.string('average_temp');
-      table.string('distance_from_sun');
+      table.string('perihelion');
+      table.string('aphelion');
       table.string('diameter');
+      table.string('gravity');
       table.string('length_of_day');
       table.integer('number_of_moons');
       table.boolean('moon');
       table.string('orbital_period');
       table.boolean('planet');
+      table.boolean('star');
       table.timestamps(true, true);
     }),
 
     knex.schema.createTable('images', table => {
       table.increments('id').primary();
       table.string('image_id');
+      table.string('image_name');
       table.integer('object_id').unsigned()
       table.foreign('object_id')
         .references('objects.id');
